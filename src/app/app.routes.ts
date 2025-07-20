c00cf262a8631f63f6d129ce1a100d3aa0edb831
HP5000SERIES\avila <avilameza@gmail.com>
2025-07-20 16:51:05 -0500

    Definiendo los directorios y rutas de navegación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { FullscreenMapPageComponent } from './pages/fullscreen-map-page/fullscreen-map-page.component';
+import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
+import { HosesPageComponent } from './pages/hoses-page/hoses-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'fullscreen',
+    component:FullscreenMapPageComponent,
+    title:'FullScreen Map'
+  },
+    {
+    path:'markers',
+    component:MarkersPageComponent,
+    title:'Bookmarks'
+  },
+    {
+    path:'houses',
+    component:HosesPageComponent,
+    title:'Available Properties'
+  },
+    {
+    path:'**',
+    redirectTo:'fullscreen'
+  },
+];
---
+++ src/app/pages/hoses-page/hoses-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hoses-page',
+  imports: [],
+  templateUrl: './hoses-page.component.html',
+})
+export class HosesPageComponent { }
